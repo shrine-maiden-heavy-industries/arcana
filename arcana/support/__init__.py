786d24be8c34c2b63eff5475f966e299229e2934
Aki Van Ness <aki@lethalbit.net>
2022-07-17 14:35:55 -0400

    added support submodule

---
+++ arcana/support/__init__.py
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: BSD-3-Clause
+
+__doc__ = '''\
+
+Arcana support machinery.
+
+
+This module provides support for things like building cross-compiler toolchains
+and other various tasks that don't fit well anywhere else.
+
+'''
+
+__all__ = (
+
+)

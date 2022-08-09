87d9b69fcd7d804052be1ece5477e15398f75c87
Aki Van Ness <aki@lethalbit.net>
2022-08-09 17:25:40 -0400

    arcana: config: added new default directories

--- arcana/config.py
+++ arcana/config.py
@@ -10,6 +10,7 @@ XDG_HOME        = Path.home()                     if 'XDG_HOME'        not in en
 XDG_CACHE_DIR   = (XDG_HOME / '.cache')           if 'XDG_CACHE_HOME'  not in environ else Path(environ['XDG_CACHE_HOME'])
 XDG_DATA_HOME   = (XDG_HOME / '.local' / 'share') if 'XDG_DATA_HOME'   not in environ else Path(environ['XDG_DATA_HOME'])
 XDG_CONFIG_HOME = (XDG_HOME / '.config')          if 'XDG_CONFIG_HOME' not in environ else Path(environ['XDG_CONFIG_HOME'])
+XDG_BIN_HOME    = (XDG_HOME / '.local' / 'bin')
 
 # Arcana-specific sub dirs
 ARCANA_CACHE   = (XDG_CACHE_DIR   / PKG_NAME)
@@ -19,3 +20,4 @@ ARCANA_CONFIG  = (XDG_CONFIG_HOME / PKG_NAME)
 ARCANA_USER_TARGETS    = (ARCANA_DATA / 'targets'   )
 ARCANA_USER_EXTENSIONS = (ARCANA_DATA / 'extensions')
 ARCANA_USER_TEMPLATES  = (ARCANA_DATA / 'templates' )
+ARCANA_TOOLCHAINS      = (ARCANA_DATA / 'toolchains')
